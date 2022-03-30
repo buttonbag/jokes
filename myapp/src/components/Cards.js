95c7d877bf239595a007752366c471a0d8eee9b1
louie C <louiecamacho@gmail.com>
2022-03-29 22:51:43 -0500

    start of structure

---
+++ myapp/src/components/Cards.js
@@ -0,0 +1,14 @@
+export default function Cards() {
+	return (
+		<div className="card">
+			<div className="card--badge">nsfw</div>
+			<p className="card--setup">
+				Setup: Nisl lacus amet et proin id enim, vel. Nisi tellus adipiscing
+				libero non neque massa.
+			</p>
+			<p className="card--punchline">
+				Punchline: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+			</p>
+		</div>
+	);
+}
